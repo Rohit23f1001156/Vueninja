a82dac2bcf0b78c366c43f539777af26256d64f0
Rohit23f1001156 <23f1001156@ds.study.iitm.ac.in>
2025-07-19 18:25:20 +0000

    till modal-project starting

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ we start, by creating a vue app, it is the job of the bue app to control either 
 with the Vue app var we created, we get access to the method called mount:
 mount method tells the app, at what point in the DOM to mount this app.
 
+a computed property is a property that is computed based on other properties,
+ it is like a method, but it is cached based on its dependencies, 
+ so it will only re-compute when one of its dependencies changes.
 */
 const app = Vue.createApp({         // we pass an object to createApp method,
     data(){
         return { 
+            //PART 4
+            url: "https://vuejs.org/",  
             showBooks: true,                   // return an object from the data method
             //PART3
             books:[
-                { title: "The Final Empire", author: "Brandon Sanderson" },  // each book is an object with properties
-                { title: "The Well of Ascension", author: "Brandon Sanderson"},
-                { title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling"},
-                { title: "The Hobbit", author: "J.R.R. Tolkien"},
-                { title: "The Name of the Wind", author: "Patrick Rothfuss"},
+                { title: "The Final Empire", author: "Brandon Sanderson" , image : 'assets/1.jpg', isFav: true},  // each book is an object with properties
+                { title: "The Well of Ascension", author: "Brandon Sanderson", image : 'assets/2.jpg', isFav: false},
+                { title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling", image : 'assets/3.jpg', isFav: true},
+                { title: "The Hobbit", author: "J.R.R. Tolkien", isFav: true},
+                { title: "The Name of the Wind", author: "Patrick Rothfuss", isFav: false},
             ]
             
             // PART 1,2
@@ -47,9 +52,17 @@ const app = Vue.createApp({         // we pass an object to createApp method,
             console.log("Mouse is moving!");  // log a message when the mouse is moving over the element
             this.x = e.offsetX;  // get the x coordinate of the mouse relative to the element
             this.y = e.offsetY;  // get the y coordinate of the mouse relative to
+        },
+        changeFav(book){
+            book.isFav = !book.isFav
         }
 
-    }
+    },
+    computed: {                   // computed properties are properties that are computed based on other properties
+        filteredBooks(){           // this is a computed property that filters the books based on the showBooks variable
+            return this.books.filter((book) => book.isFav);  // return only the books that are marked as favorite
+        }
+    },
 })
 app.mount("#app")   // mount the app to the element with id "app"
 console.log("Hello Vue")
---
+++ modal_notes.js
@@ -0,0 +1,15 @@
+/*
+talking about modal-project folder
+inside modal-project folder
+node modules folder:
+this is where dependencies are stored
+package.json file:
+this is where the dependencies are listed
+package-lock.json file:
+this is where the exact versions of the dependencies are stored
+
+public folder:
+
+
+
+*/
